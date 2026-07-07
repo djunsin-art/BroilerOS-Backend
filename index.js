4ae8c8d69b61fae1f1aa3a19e56e7916f0678ab2
Hemita <djunsin@gmail.com>
2026-07-08 01:40:30 +0700

    Configure DNS to prefer IPv4
    Set default DNS lookup order to IPv4.

--- index.js
+++ index.js
@@ -49,6 +49,12 @@ if (!process.env.DATABASE_URL) {
     console.error('⚠️ Server tetap berjalan, tetapi endpoint database akan error.');
 }
 
+const { Pool } = require('pg');
+const dns = require('dns');
+
+// Set default lookup ke IPv4
+dns.setDefaultResultOrder('ipv4first');
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
