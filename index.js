a638b7f6529869b948f12b3997182728b17cf81a
Hemita <djunsin@gmail.com>
2026-07-08 17:48:33 +0700

    Remove entry from allowed origins in CORS
    Removed a comment and an entry from the allowed origins list in the CORS configuration.

--- index.js
+++ index.js
@@ -30,8 +30,7 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:3000',
     'https://broileros.onrender.com'
-    'https://*.workers.dev', // <-- Tambahkan ini
-];
+    ];
 app.use(cors({
     origin: function (origin, callback) {
         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
